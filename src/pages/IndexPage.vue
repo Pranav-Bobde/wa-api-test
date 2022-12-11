b2e48a930696b88dfeb0ef2d1e6fd959b9ab8fe6
Pranav Bobde <bobdep31@gmail.com>
2022-12-12 03:33:00 +0530

    first commit with wa redirect api

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    <q-btn
+      color="white"
+      text-color="black"
+      label="Standard"
+      @click="() => sendMsg('+919370892274', 'Hello World')"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  methods: {
+    async getPosts() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const posts = await response.json();
+      console.log(posts);
+    },
+    sendMsg(number, message) {
+      console.log("number", number);
+      console.log("message", message);
+      var url =
+        "https://api.whatsapp.com/send?phone=" +
+        number +
+        "&text=" +
+        encodeURIComponent(message);
+
+      (async () => {
+        try {
+          window.open(url, "_blank").focus();
+        } catch (error) {
+          console.log("error", error);
+        }
+      })();
+      // take the user to whatsapp and open the chat with the number
+
+      return url;
+    },
+  },
+});
+</script>
